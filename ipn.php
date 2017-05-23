64da9034380a7242e53e18fe36ff42019f22ab3f
Andy Theuninck <andy@gohanman.com>
2017-05-23 14:29:47 -0700

    Theoretically equity implementation for IPN callbacks

--- ipn.php
+++ ipn.php
@@ -18,3 +18,41 @@ if ($ipn->validate()) {
 }
 fclose($fp);
 
+/**
+  Run an equity transaction for the notified payment
+*/
+function addEquityPayment($profileID, $amount)
+{
+    $dbc = Database::pDataConnect();
+    $prep = $dbc->prepare_statement('SELECT cardNo, email FROM PaymentProfiles WHERE profileID=?');
+    $res = $dbc->exec_statement($prep, array($profileID));
+    $row = $dbc->fetch_row($res);
+    if (!$row) {
+        return false;
+    }
+    $card_no = $row['cardNo'];
+    $email = $row['email'];
+    AuthUtilities::doLogin($email);
+    $empno = AuthUtilities::getUID($email);
+    TransRecord::addOpenRing($amount, 991, 'Class B Equity');
+    $pay_class = RemoteProcessor::CURRENT_PROCESSOR;
+    $proc = new $pay_class();
+    TransRecord::addtender($proc->tender_name, $proc->tender_code, -1*$amount);
+
+    $endP = $dbc->prepare_statement("INSERT INTO localtrans SELECT l.* FROM
+        localtemptrans AS l WHERE emp_no=?");
+    $endR = $dbc->exec_statement($endP,array($empno));
+
+    $pendingCols = Database::localMatchingColumns($dbc, 'localtemptrans', 'pendingtrans');
+    $endP = $dbc->prepare_statement("INSERT INTO pendingtrans ($pendingCols) 
+                                    SELECT $pendingCols 
+                                    FROM localtemptrans AS l 
+                                    WHERE l.emp_no=?");
+
+    $endR = $dbc->exec_statement($endP,array($empno));
+    if ($endR !== false) {
+        $clearP = $dbc->prepare_statement("DELETE FROM localtemptrans WHERE emp_no=?");
+        $dbc->exec_statement($clearP,array($empno));
+    }
+}
+
