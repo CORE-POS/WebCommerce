701f5ad711a7d2a3d7d34a8bb1c01bc2ee59d67c
Andy Theuninck <andy@gohanman.com>
2020-10-30 10:24:00 -0700

    Update productOrderLimit via pendingtrans task

--- cron/pending.php
+++ cron/pending.php
@@ -15,14 +15,33 @@ include('../ini.php');
 $db = Database::tDataConnect();
 
 $db->query("LOCK TABLES pendingtrans WRITE, dtransactions WRITE, 
-	localtrans_today WRITE");
+	localtrans_today WRITE, productOrderLimits WRITE");
 
 // get upcs & quantities from pending
-$data = array();
-$result = $db->query("SELECT upc,sum(quantity) as qty FROM pendingtrans
-		WHERE trans_type='I'");
-while($row = $db->fetch_row($result)){
-	$data[$row['upc']] = $row['qty'];
+$transR = $db->query("SELECT emp_no, register_no, trans_no FROM pendingtrans GROUP BY emp_no, register_no, trans_no");
+while ($transW = $db->fetchRow($transR)) {
+    $data = array();
+    $result = $db->query(sprintf("SELECT upc,sum(quantity) as qty FROM pendingtrans
+            WHERE trans_type='I' AND emp_no=%d AND register_no=%d AND trans_no=%d
+            GROUP BY upc",
+            $transW['emp_no'], $transW['register_no'], $transW['trans_no']));
+    while($row = $db->fetch_row($result)){
+        $data[$row['upc']] = $row['qty'];
+    }
+    $storeR = $db->query(sprintf("SELECT description FROM pendingtrans
+        WHERE emp_no=%d AND register_no=%d AND trans_no=%d AND trans_type='C'
+            AND trans_subtype='CM' AND description LIKE 'STORE%%'",
+            $transW['emp_no'], $transW['register_no'], $transW['trans_no']));
+    $storeID = null;
+    while ($storeW =  $db->fetchRow($storeR)) {
+        list($nothing, $storeID) = explode(' ', $storeW['description']);
+    }
+    foreach($data as $upc=>$qty){
+        $q = sprintf("UPDATE productOrderLimits 
+            SET available=available-%d
+            WHERE upc='%s' AND storeID=%d",$qty,$upc,$storeID);
+        $r = $db->query($q);
+    }
 }
 
 // shuffle contents to final trans tables
@@ -40,12 +59,6 @@ $db->query("UNLOCK TABLES");
 
 // update limits based on amounts sold
 $db2 = Database::pDataConnect();
-foreach($data as $upc=>$qty){
-	$q = sprintf("UPDATE productOrderLimits 
-		SET available=available-%d
-		WHERE upc='%s'",$qty,$upc);
-	$r = $db2->query($q);
-}
 
 $endQ = "UPDATE products AS p INNER JOIN
 	productOrderLimits AS l ON p.upc=l.upc
